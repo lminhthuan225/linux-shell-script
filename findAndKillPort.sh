53b2b5b59d5fd75854efeb628a1e12ce8c9622b4
skqist225 <74346301+skqist225@users.noreply.github.com>
2022-04-15 21:30:06 +0700

    echo no port running if PID = null

--- findAndKillPort.sh
+++ findAndKillPort.sh
@@ -3,5 +3,6 @@
 
 PORT_NUMBER="$1"
 PORT_PID=$(lsof -i -P -n | grep $PORT_NUMBER | egrep "[0-9]" | uniq)
-
+[[ -z $PORT_PID ]] && echo "No service running on this port" ||  (
+sudo kill $PORT_PID)
 echo $PORT_PID
